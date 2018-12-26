7752ffb3f980c2b306b510f9ca89eb4bc9db742b
h4stoor <h4stoor@gmail.com>
2018-12-26 21:06:55 +0100

    models for card and cards group

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class FlashCardsGroup(models.Model):
+    title = models.CharField(max_length=120)
+
+
+class FlashCard(models.Model):
+    title = models.CharField(max_length=120)
+    category = models.ForeignKey(FlashCardsGroup, on_delete=models.CASCADE, related_name='category')
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='author')
