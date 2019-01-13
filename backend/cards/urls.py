850a8a520e1115abdc7c3a9e2eee94e01f651c62
h4stoor <h4stoor@gmail.com>
2019-01-13 19:30:30 +0100

    add rest_framework_nested and create routers for questions and answers

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -1,10 +1,17 @@
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 
-from cards.views import FlashCardCategoryViewSet
-
-
-router = routers.DefaultRouter()
-router.register('categories', FlashCardCategoryViewSet, base_name='category')
+from cards.views import FlashCardCategoryViewSet, FlashCardQuestionViewSet, FlashCardAnswerViewSet
 
 
-urlpatterns = router.urls
+cards_router = DefaultRouter()
+cards_router.register('categories', FlashCardCategoryViewSet, basename='category')
+
+questions_router = routers.NestedDefaultRouter(cards_router, 'categories', lookup='category')
+questions_router.register('questions', FlashCardQuestionViewSet, basename='question')
+
+answers_router = routers.NestedDefaultRouter(questions_router, 'questions', lookup='question')
+answers_router.register('answers', FlashCardAnswerViewSet, basename='answers')
+
+
+urlpatterns = cards_router.urls + questions_router.urls + answers_router.urls
