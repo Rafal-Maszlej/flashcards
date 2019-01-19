8f9ac0e8292acb1ee61c4d33bb32cad7f9f726d8
h4stoor <h4stoor@gmail.com>
2019-01-19 18:35:31 +0100

    add CustomUser to admin panel, create fixtures for users and accounts

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import Account, CustomUser
+
+
+class AccountAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Account
+
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Account, AccountAdmin)
