cb0bd63811ee0f1646619b0595b54b8e513f6391
h4stoor <h4stoor@gmail.com>
2019-01-19 18:52:59 +0100

    endpoint for authenticated user account details

--- backend/accounts/tests/test_account.py
+++ backend/accounts/tests/test_account.py
@@ -8,27 +8,46 @@ from accounts.tests.factories import AccountFactory
 class AccountsListTestCase(test.APITestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.account_list_url = reverse('accounts-list')
+        cls.accounts_list_url = reverse('accounts-list')
 
         cls.admin_account = AccountFactory(user__is_staff=True)
         cls.non_admin_account = AccountFactory()
 
     def test_unauthenticated_user_cannot_get_accounts_list(self):
-        response = self.client.get(self.account_list_url)
+        response = self.client.get(self.accounts_list_url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_non_admin_cannot_get_accounts_list(self):
         self.client.force_login(self.non_admin_account.user)
 
-        response = self.client.get(self.account_list_url)
+        response = self.client.get(self.accounts_list_url)
 
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_admin_get_accounts_list(self):
         self.client.force_login(self.admin_account.user)
 
-        response = self.client.get(self.account_list_url)
+        response = self.client.get(self.accounts_list_url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), Account.objects.count())
+
+
+class AccountMyTestCase(test.APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.accounts_my_url = reverse('accounts-my')
+
+        cls.account1 = AccountFactory()
+        cls.account2 = AccountFactory()
+
+    def setUp(self):
+        self.client.force_login(self.account1.user)
+
+    def test_user_retrieve_own_account_details(self):
+        response = self.client.get(self.accounts_my_url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('user', response.data)
+        self.assertEqual(response.data['user'], self.account1.pk)
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 
 from accounts.models import Account
 from accounts.serializers import AccountSerializer
@@ -9,3 +11,8 @@ class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
     permission_classes = (IsAdminUser,)
+
+    @action(methods=['GET'], detail=False, permission_classes=(IsAuthenticated,))
+    def my(self, request):
+        account = request.user.account
+        return Response(self.get_serializer_class()(account).data)
